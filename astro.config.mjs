30cb102673aeb699316e7589e23a9380385d01ff
AvoMelkonian <melkonianavo7@gmail.com>
2025-10-17 15:30:42 +0300

    new file creation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import { defineCollection, z } from 'astro:content';
+
+const postsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    category: z.string(),
+  }),
+});
+
+export default defineConfig({
+  integrations: [],
+  content: {
+    collections: {
+      posts: postsCollection,
+    },
+  },
+});
