e978e0f339c5c3a71b55c356f71aea5ae852dba9
AvoMelkonian <melkonianavo7@gmail.com>
2025-10-18 23:45:13 +0300

    Created New Build

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'نظم الوسائط المتعددة',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Hello World', slug: 'guides/hello-world' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
