41d0bdf3dc2a4f4c91e2f43836f4ebfc937e2a95
AvoMelkonian <melkonianavo7@gmail.com>
2025-10-20 13:46:09 +0300

    Added Font

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'نظم الوسائط المتعددة',
+			customCss: ['./src/styles/custom.css'],
 			defaultLocale: 'root',
 			locales: {
 				root: {
